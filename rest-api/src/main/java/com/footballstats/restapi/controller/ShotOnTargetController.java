020416eb22358eb3f10c0cba22b583649efbd6c3
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-13 22:04:55 +0200

    Clean up REST API for shots on target
    Remove unnecessary league parameter in a method. Rearrange method names.

--- rest-api/src/main/java/com/footballstats/restapi/controller/ShotOnTargetController.java
+++ rest-api/src/main/java/com/footballstats/restapi/controller/ShotOnTargetController.java
@@ -23,19 +23,18 @@ public class ShotOnTargetController {
 
     @GetMapping("/avg_league/{" + LEAGUE + "}")
     public double getAvgForLeague(@PathVariable(LEAGUE) String league) {
-        return dao.getAvgShotsOnTarget(league);
+        return dao.getAvgForLeague(league);
     }
 
     @GetMapping("/avg_league_season/{" + LEAGUE + "}/{" + SEASON + "}")
     public double getAvgForLeagueAndSeason(@PathVariable(LEAGUE) String league,
                                            @PathVariable(SEASON) String season) {
-        return dao.getAvgShotsOnTarget(league, season);
+        return dao.getAvgForLeagueAndSeason(league, season);
     }
 
-    @GetMapping("/avg_league_season_game/{" + LEAGUE + "}/{" + SEASON + "}/{" + TEAM + "}")
-    public double getAvgForLeagueSeasonAndTeam(@PathVariable(LEAGUE) String league,
-                                               @PathVariable(SEASON) String season,
-                                               @PathVariable(TEAM) String team) {
-        return dao.getAvgShotsOnTarget(league, season, team);
+    @GetMapping("/avg_season_game/{" + SEASON + "}/{" + TEAM + "}")
+    public double getAvgForSeasonAndGame(@PathVariable(SEASON) String season,
+                                         @PathVariable(TEAM) String team) {
+        return dao.getAvgForSeasonAndTeam(season, team);
     }
 }
--- rest-api/src/main/java/com/footballstats/restapi/dao/ShotOnTargetDao.java
+++ rest-api/src/main/java/com/footballstats/restapi/dao/ShotOnTargetDao.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 public class ShotOnTargetDao {
     private static final String TABLE_NAME = "european_league_results.game_results";
     private static final String SEASON = "season";
-    private static final String LEAGUE = "league";
     private static final String TEAM = "team";
 
     private Session session;
@@ -20,37 +19,36 @@ public class ShotOnTargetDao {
         session = connector.getSession();
     }
 
-    public double getAvgShotsOnTarget(String league, String season, String team) {
-        SumAndCount home = getShotsOnTargetSumAndCount(season, league, team, true);
-        SumAndCount away = getShotsOnTargetSumAndCount(season, league, team, false);
+    public double getAvgForSeasonAndTeam(String season, String team) {
+        SumAndCount home = getShotsOnTargetSumAndCount(season, team, true);
+        SumAndCount away = getShotsOnTargetSumAndCount(season, team, false);
 
         return (1.0 * (home.getSum() + away.getSum())) / (1.0 * (home.getCount() + away.getCount()));
     }
 
-    public double getAvgShotsOnTarget(String league) {
+    public double getAvgForLeague(String league) {
         String query = "select avg(sum_shots_target) from " + TABLE_NAME + " where league='" + league + "' allow filtering;";
         ResultSet resultSet = session.execute(query);
         return resultSet.one().getDouble(0);
     }
 
-    public double getAvgShotsOnTarget(String league, String season) {
+    public double getAvgForLeagueAndSeason(String league, String season) {
         String query = "select avg(sum_shots_target) from " + TABLE_NAME + " where league='" + league + "' and season='" + season + "' allow filtering;";
         ResultSet resultSet = session.execute(query);
         return resultSet.one().getDouble(0);
     }
 
-    private SumAndCount getShotsOnTargetSumAndCount(String season, String league, String team, boolean homeTeam) {
+    private SumAndCount getShotsOnTargetSumAndCount(String season, String team, boolean homeTeam) {
         String query = "";
         if (homeTeam) {
-            query = "select sum(sum_shots_target), count(sum_shots_target) from " + TABLE_NAME + " where season=:season and league=:league and home_team=:team allow filtering";
+            query = "select sum(sum_shots_target), count(sum_shots_target) from " + TABLE_NAME + " where season=:season and home_team=:team allow filtering";
         } else {
-            query = "select sum(sum_shots_target), count(sum_shots_target) from " + TABLE_NAME + " where season=:season and league=:league and away_team=:team allow filtering";
+            query = "select sum(sum_shots_target), count(sum_shots_target) from " + TABLE_NAME + " where season=:season and away_team=:team allow filtering";
         }
         PreparedStatement preparedStatement = session.prepare(query);
 
         BoundStatement boundStatement = preparedStatement.bind()
                 .setString(SEASON, season)
-                .setString(LEAGUE, league)
                 .setString(TEAM, team);
 
         ResultSet resultSet = session.execute(boundStatement);
