e88a6a4a3c1baac9b4766e497520b04d1745b0b0
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-13 20:06:09 +0200

    Create REST controller for corners

---
+++ rest-api/src/main/java/com/footballstats/restapi/controller/CornerController.java
@@ -0,0 +1,41 @@
+package com.footballstats.restapi.controller;
+
+import com.footballstats.restapi.dao.CornerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/corners")
+public class CornerController {
+    private static final String LEAGUE = "league";
+    private static final String SEASON = "season";
+    private static final String TEAM = "team";
+
+    private CornerDao dao;
+
+    @Autowired
+    CornerController(CornerDao dao) {
+        this.dao = dao;
+    }
+
+    @GetMapping("/avg_league/{" + LEAGUE + "}")
+    public double getAvgForLeague(@PathVariable(LEAGUE) String league) {
+        return dao.getAvgCorners(league);
+    }
+
+    @GetMapping("/avg_league_season/{" + LEAGUE + "}/{" + SEASON + "}")
+    public double getAvgForLeagueAndSeason(@PathVariable(LEAGUE) String league,
+                                           @PathVariable(SEASON) String season) {
+        return dao.getAvgCorners(league, season);
+    }
+
+    @GetMapping("/avg_league_season_team/{" + LEAGUE + "}/{" + SEASON + "}/{" + TEAM + "}")
+    public double getAvgForLeagueSeasonAndTeam(@PathVariable(LEAGUE) String league,
+                                               @PathVariable(SEASON) String season,
+                                               @PathVariable(TEAM) String team) {
+        return dao.getAvgCorners(league, season, team);
+    }
+}
