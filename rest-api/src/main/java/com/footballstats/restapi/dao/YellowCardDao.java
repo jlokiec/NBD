927c3b0022a38533c957d2e818dab40f89c7d0ca
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-13 22:24:49 +0200

    Clean up REST API for yellow cards
    Remove unnecessary league parameter in a method. Rearrange method names.

--- rest-api/src/main/java/com/footballstats/restapi/dao/YellowCardDao.java
+++ rest-api/src/main/java/com/footballstats/restapi/dao/YellowCardDao.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 public class YellowCardDao {
     private static final String TABLE_NAME = "european_league_results.game_results";
     private static final String SEASON = "season";
-    private static final String LEAGUE = "league";
     private static final String TEAM = "team";
 
     private Session session;
@@ -20,37 +19,36 @@ public class YellowCardDao {
         session = connector.getSession();
     }
 
-    public double getAvgYellowCards(String league, String season, String team) {
-        SumAndCount home = getYellowCardsSumAndCount(season, league, team, true);
-        SumAndCount away = getYellowCardsSumAndCount(season, league, team, false);
+    public double getAvgForSeasonAndTeam(String season, String team) {
+        SumAndCount home = getYellowCardsSumAndCount(season, team, true);
+        SumAndCount away = getYellowCardsSumAndCount(season, team, false);
 
         return (1.0 * (home.getSum() + away.getSum())) / (1.0 * (home.getCount() + away.getCount()));
     }
 
-    public double getAvgYellowCards(String league) {
+    public double getAvgForLeague(String league) {
         String query = "select avg(sum_yellow) from " + TABLE_NAME + " where league='" + league + "' allow filtering;";
         ResultSet resultSet = session.execute(query);
         return resultSet.one().getDouble(0);
     }
 
-    public double getAvgYellowCards(String league, String season) {
+    public double getAvgForLeagueAndSeason(String league, String season) {
         String query = "select avg(sum_yellow) from " + TABLE_NAME + " where league='" + league + "' and season='" + season + "' allow filtering;";
         ResultSet resultSet = session.execute(query);
         return resultSet.one().getDouble(0);
     }
 
-    private SumAndCount getYellowCardsSumAndCount(String season, String league, String team, boolean homeTeam) {
+    private SumAndCount getYellowCardsSumAndCount(String season, String team, boolean homeTeam) {
         String query = "";
         if (homeTeam) {
-            query = "select sum(sum_yellow), count(sum_yellow) from " + TABLE_NAME + " where season=:season and league=:league and home_team=:team allow filtering";
+            query = "select sum(sum_yellow), count(sum_yellow) from " + TABLE_NAME + " where season=:season and home_team=:team allow filtering";
         } else {
-            query = "select sum(sum_yellow), count(sum_yellow) from " + TABLE_NAME + " where season=:season and league=:league and away_team=:team allow filtering";
+            query = "select sum(sum_yellow), count(sum_yellow) from " + TABLE_NAME + " where season=:season and away_team=:team allow filtering";
         }
         PreparedStatement preparedStatement = session.prepare(query);
 
         BoundStatement boundStatement = preparedStatement.bind()
                 .setString(SEASON, season)
-                .setString(LEAGUE, league)
                 .setString(TEAM, team);
 
         ResultSet resultSet = session.execute(boundStatement);
