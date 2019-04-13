4be4a040717349845d2158e08d6cb904ff89e04e
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-13 23:00:32 +0200

    Create DAO for over/under goals

---
+++ rest-api/src/main/java/com/footballstats/restapi/dao/GoalDao.java
@@ -0,0 +1,59 @@
+package com.footballstats.restapi.dao;
+
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import com.footballstats.restapi.cassandra.CassandraConnector;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GoalDao {
+    private static final String TABLE_NAME = "european_league_results.game_results";
+    private static final String SEASON = "season";
+    private static final String TEAM = "team";
+
+    private Session session;
+
+    public GoalDao() {
+        CassandraConnector connector = new CassandraConnector();
+        connector.connect();
+        session = connector.getSession();
+    }
+
+    public long countGamesOverForLeague(String league, double limit) {
+        String query = "select count(*) from " + TABLE_NAME + " where league='" + league + "' and ft_sum_goals > " + limit + " allow filtering;";
+        ResultSet resultSet = session.execute(query);
+        return resultSet.one().getLong(0);
+    }
+
+    public long countGamesOverForLeagueAndSeason(String league, String season, double limit) {
+        String query = "select count(*) from " + TABLE_NAME + " where league='" + league + "' and season='" + season + "' and ft_sum_goals > " + limit + " allow filtering;";
+        ResultSet resultSet = session.execute(query);
+        return resultSet.one().getLong(0);
+    }
+
+    public long countGamesOverForSeasonAndTeam(String season, String team, double limit) {
+        long countHome = countGamesWithGoalsOver(season, team, limit, true);
+        long countAway = countGamesWithGoalsOver(season, team, limit, false);
+
+        return countHome + countAway;
+    }
+
+    private long countGamesWithGoalsOver(String season, String team, double limit, boolean homeTeam) {
+        String query = "";
+        if (homeTeam) {
+            query = "select count(*) from " + TABLE_NAME + " where season=:season and home_team=:team and ft_sum_goals > " + limit + " allow filtering";
+        } else {
+            query = "select count(*) from " + TABLE_NAME + " where season=:season and away_team=:team and ft_sum_goals > " + limit + " allow filtering";
+        }
+        PreparedStatement preparedStatement = session.prepare(query);
+
+        BoundStatement boundStatement = preparedStatement.bind()
+                .setString(SEASON, season)
+                .setString(TEAM, team);
+
+        ResultSet resultSet = session.execute(boundStatement);
+        return resultSet.one().getLong(0);
+    }
+}
