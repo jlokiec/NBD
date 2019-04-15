bec2306e3e955de4f99c31fa605fc1ecc2de4f99
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-15 22:16:05 +0200

    Improve csv-to-cql.py script
    Add keyspace info, improve code formatting and print info about appended
    insert statements.

--- scripts/csv-to-cql.py
+++ scripts/csv-to-cql.py
@@ -55,16 +55,22 @@ if __name__ == '__main__':
                         away_red = row[20]
                         sum_red = int(home_red)+int(away_red)
 
-                        query = 'insert into game_results(uid, date, season, league, home_team, away_team, ft_home_goals, ft_away_goals, '\
-                            'ft_sum_goals, ft_result, ht_home_goals, ht_away_goals, ht_sum_goals, ht_result, home_shots, '\
-                            'away_shots, sum_shots, home_shots_target, away_shots_target, sum_shots_target, home_fouls, '\
-                            'away_fouls, sum_fouls, home_corners, away_corners, sum_corners, home_yellow, away_yellow, '\
-                            'sum_yellow, home_red, away_red, sum_red) values ({0}, \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', {6}, {7}, {8}, '\
-                            '\'{9}\', {10}, {11}, {12}, \'{13}\', {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, '\
-                            '{25}, {26}, {27}, {28}, {29}, {30}, {31});\n'.format(uid, date, season, league, home_team, away_team, ft_home_goals,
-                                                                                  ft_away_goals, ft_sum_goals, ft_result, ht_home_goals, ht_away_goals, ht_sum_goals, ht_result, home_shots, away_shots,
-                                                                                  sum_shots, home_shots_target, away_shots_target, sum_shots_target, home_fouls, away_fouls, sum_fouls,
-                                                                                  home_corners, away_corners, sum_corners, home_yellow, away_yellow, sum_yellow, home_red, away_red, sum_red)
+                        query = 'insert into european_league_results.game_results(uid, date, season, league, home_team, away_team, '\
+                            'ft_home_goals, ft_away_goals, ft_sum_goals, ft_result, ht_home_goals, ht_away_goals, ht_sum_goals, '\
+                            'ht_result, home_shots, away_shots, sum_shots, home_shots_target, away_shots_target, sum_shots_target, '\
+                            'home_fouls, away_fouls, sum_fouls, home_corners, away_corners, sum_corners, home_yellow, away_yellow, '\
+                            'sum_yellow, home_red, away_red, sum_red) values ({0}, \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', {6}, '\
+                            '{7}, {8}, \'{9}\', {10}, {11}, {12}, \'{13}\', {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, '\
+                            '{23}, {24}, {25}, {26}, {27}, {28}, {29}, {30}, {31});\n'.format(uid, date, season, league, home_team,
+                                                                                              away_team, ft_home_goals, ft_away_goals,
+                                                                                              ft_sum_goals, ft_result, ht_home_goals,
+                                                                                              ht_away_goals, ht_sum_goals, ht_result,
+                                                                                              home_shots, away_shots, sum_shots,
+                                                                                              home_shots_target, away_shots_target,
+                                                                                              sum_shots_target, home_fouls, away_fouls,
+                                                                                              sum_fouls, home_corners, away_corners,
+                                                                                              sum_corners, home_yellow, away_yellow,
+                                                                                              sum_yellow, home_red, away_red, sum_red)
 
                         queries.append(query)
                     else:
@@ -72,6 +78,9 @@ if __name__ == '__main__':
 
             with open(output_filename, 'a') as cqlfile:
                 cqlfile.writelines(queries)
+
+            print(
+                f'Appended {len(queries)} insert statements to {output_filename}.')
         else:
             print('Given file is not a CSV file. Exiting.')
     else:
