623b249775e550ebca3342a9ad10662520b695a9
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-18 15:02:52 +0200

    Use Lombok Data annotation where it makes sense

--- rest-api/src/main/java/com/footballstats/restapi/model/HomeAwaySum.java
+++ rest-api/src/main/java/com/footballstats/restapi/model/HomeAwaySum.java
@@ -1,5 +1,8 @@
 package com.footballstats.restapi.model;
 
+import lombok.Data;
+
+@Data
 public class HomeAwaySum {
     private double home;
     private double away;
@@ -11,30 +14,6 @@ public class HomeAwaySum {
     public HomeAwaySum(double home, double away, double sum) {
         this.home = home;
         this.away = away;
-        this.sum = sum;
-    }
-
-    public double getHome() {
-        return home;
-    }
-
-    public void setHome(double home) {
-        this.home = home;
-    }
-
-    public double getAway() {
-        return away;
-    }
-
-    public void setAway(double away) {
-        this.away = away;
-    }
-
-    public double getSum() {
-        return sum;
-    }
-
-    public void setSum(double sum) {
         this.sum = sum;
     }
 }
