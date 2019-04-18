d9d3425044a2bcca386c7fcf6997b3650e4a4db4
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-18 13:43:22 +0200

    Use cached PreparedStatements in ResultDao

--- rest-api/src/main/java/com/footballstats/restapi/dao/ResultDao.java
+++ rest-api/src/main/java/com/footballstats/restapi/dao/ResultDao.java
@@ -1,10 +1,10 @@
 package com.footballstats.restapi.dao;
 
 import com.datastax.driver.core.BoundStatement;
-import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
-import com.footballstats.restapi.cassandra.CassandraConnector;
+import com.footballstats.restapi.cassandra.CassandraConfiguration;
+import com.footballstats.restapi.cassandra.PreparedStatementCache;
 import com.footballstats.restapi.dao.util.GameResult;
 import com.footballstats.restapi.dao.util.TeamResults;
 import org.springframework.stereotype.Repository;
@@ -13,13 +13,20 @@ import javax.annotation.Resource;
 
 @Repository
 public class ResultDao {
-    private static final String TABLE_NAME = "european_league_results.game_results";
     private static final String SEASON = "season";
     private static final String RESULT = "result";
     private static final String TEAM = "team";
 
-    @Resource(name = CassandraConnector.SESSION)
+    private static final String COUNT_FOR_HOME_TEAM_RESULT = "select count(ft_result) from game_results where home_team=:team and ft_result=:result allow filtering";
+    private static final String COUNT_FOR_AWAY_TEAM_RESULT = "select count(ft_result) from game_results where away_team=:team and ft_result=:result allow filtering";
+    private static final String COUNT_FOR_HOME_TEAM_SEASON_RESULT = "select count(ft_result) from game_results where season=:season and home_team=:team and ft_result=:result allow filtering";
+    private static final String COUNT_FOR_AWAY_TEAM_SEASON_RESULT = "select count(ft_result) from game_results where season=:season and away_team=:team and ft_result=:result allow filtering";
+
+    @Resource(name = CassandraConfiguration.SESSION)
     private Session session;
+
+    @Resource(name = CassandraConfiguration.STATEMENT_CACHE)
+    private PreparedStatementCache statementCache;
 
     public TeamResults getTeamResults(String team) {
         long homeWins = countTeamResults(team, GameResult.HOME_WIN, true);
@@ -52,37 +59,35 @@ public class ResultDao {
     }
 
     public long countTeamResults(String team, GameResult result, boolean homeTeam) {
-        String query = "";
+        BoundStatement statement = null;
+
         if (homeTeam) {
-            query = "select count(ft_result) from " + TABLE_NAME + " where home_team=:team and ft_result=:result allow filtering";
+            statement = statementCache.getCqlStatement(COUNT_FOR_HOME_TEAM_RESULT);
         } else {
-            query = "select count(ft_result) from " + TABLE_NAME + " where away_team=:team and ft_result=:result allow filtering";
+            statement = statementCache.getCqlStatement(COUNT_FOR_AWAY_TEAM_RESULT);
         }
-        PreparedStatement preparedStatement = session.prepare(query);
 
-        BoundStatement boundStatement = preparedStatement.bind()
-                .setString(TEAM, team)
-                .setString(RESULT, result.getResult());
+        statement.setString(TEAM, team);
+        statement.setString(RESULT, result.getResult());
+        ResultSet resultSet = session.execute(statement);
 
-        ResultSet resultSet = session.execute(boundStatement);
         return resultSet.one().getLong(0);
     }
 
     public long countTeamResults(String team, String season, GameResult result, boolean homeTeam) {
-        String query = "";
+        BoundStatement statement = null;
+
         if (homeTeam) {
-            query = "select count(ft_result) from " + TABLE_NAME + " where season=:season and home_team=:team and ft_result=:result allow filtering";
+            statement = statementCache.getCqlStatement(COUNT_FOR_HOME_TEAM_SEASON_RESULT);
         } else {
-            query = "select count(ft_result) from " + TABLE_NAME + " where season=:season and away_team=:team and ft_result=:result allow filtering";
+            statement = statementCache.getCqlStatement(COUNT_FOR_AWAY_TEAM_SEASON_RESULT);
         }
-        PreparedStatement preparedStatement = session.prepare(query);
 
-        BoundStatement boundStatement = preparedStatement.bind()
-                .setString(SEASON, season)
-                .setString(TEAM, team)
-                .setString(RESULT, result.getResult());
+        statement.setString(TEAM, team);
+        statement.setString(SEASON, season);
+        statement.setString(RESULT, result.getResult());
+        ResultSet resultSet = session.execute(statement);
 
-        ResultSet resultSet = session.execute(boundStatement);
         return resultSet.one().getLong(0);
     }
 }
